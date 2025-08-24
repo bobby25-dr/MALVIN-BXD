86525de92878f4146d8603fa4067848f2f8a0bdb
bobby25-dr <fidelisa629@gmail.com>
2025-08-24 18:28:17 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~gm1T3bhZ#xzGKSf9e-FU4eqx-QbSf9_xdq6BxdW6cyax1h_sH1yg",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2349017117779", // put your phone number here
   
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  PREFIX: "*", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Lagos" //put your country timeZone....leave blank if u don't know.
 };
